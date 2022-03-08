e66c3e7a4e75e2ef771608ffd0be4a8e9080f5f4
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-03-08 16:58:05 +0700

    add docker-compose file and Dockerfile

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('books',\App\Http\Controllers\BookController::class);
